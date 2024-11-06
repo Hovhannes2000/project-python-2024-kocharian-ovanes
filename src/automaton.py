e53b49fa10335882224610d57c5657786ce1c89b
bendenkuzo <qgakudara@gmail.com>
2024-11-07 01:57:13 +0300

    project 1.0.0

--- src/automaton.py
+++ src/automaton.py
@@ -91,115 +91,115 @@ class NFA:
 
     #######################################################################
 
-def remove_epsilon_transitions(self):
-    # Пустой переход будем считать переходом по символу '1'
-    # Переводим НКА в НКА с однобуквенными переходами
-    if not self.is_single_letters:
-        self.make_transitions_single_letter()
+    def remove_epsilon_transitions(self):
+        # Пустой переход будем считать переходом по символу '1'
+        # Переводим НКА в НКА с однобуквенными переходами
+        if not self.is_single_letters:
+            self.make_transitions_single_letter()
 
-    new_transition_function = {}
-    # Копирую все переходы в new_transition_function
-    for state in self.states:
-        for symbol in self.alphabet:
-            if (state, symbol) in self.transition_function:
-                new_transition_function[(state, symbol)] = self.transition_function[(state, symbol)].copy()
+        new_transition_function = {}
+        # Копирую все переходы в new_transition_function
+        for state in self.states:
+            for symbol in self.alphabet:
+                if (state, symbol) in self.transition_function:
+                    new_transition_function[(state, symbol)] = self.transition_function[(state, symbol)].copy()
 
-    for state in self.states:
-        while (state, '1') in self.transition_function:
-            for next_state in new_transition_function[(state, '1')]:
-                next_states = new_transition_function[(state, '1')].copy()
-                for symbol in self.alphabet:
-                    if (next_state, symbol) in new_transition_function:
-                        if (state, symbol) not in self.transition_function:
-                            self.transition_function[(state, symbol)] = set()
-                        copy = new_transition_function[(next_state, symbol)].copy()
-                        self.transition_function[(state, symbol)] |= copy
-            self.transition_function[(state, '1')] -= next_states
-            new_transition_function.pop((state, '1'), None)
-            if not self.transition_function[(state, '1')]:
-                self.transition_function.pop((state, '1'), None)
-            if (state, '1') in self.transition_function:
-                if (state, '1') not in new_transition_function:
-                    new_transition_function[(state, '1')] = set()
-                copy = self.transition_function[(state, '1')].copy()
-                new_transition_function[(state, '1')] |= copy
+        for state in self.states:
+            while (state, '1') in self.transition_function:
+                for next_state in new_transition_function[(state, '1')]:
+                    next_states = new_transition_function[(state, '1')].copy()
+                    for symbol in self.alphabet:
+                        if (next_state, symbol) in new_transition_function:
+                            if (state, symbol) not in self.transition_function:
+                                self.transition_function[(state, symbol)] = set()
+                            copy = new_transition_function[(next_state, symbol)].copy()
+                            self.transition_function[(state, symbol)] |= copy
+                self.transition_function[(state, '1')] -= next_states
+                new_transition_function.pop((state, '1'), None)
+                if not self.transition_function[(state, '1')]:
+                    self.transition_function.pop((state, '1'), None)
+                if (state, '1') in self.transition_function:
+                    if (state, '1') not in new_transition_function:
+                        new_transition_function[(state, '1')] = set()
+                    copy = self.transition_function[(state, '1')].copy()
+                    new_transition_function[(state, '1')] |= copy
 
-    # Убираю из алфавита пустой переход
-    while '1' in self.alphabet:
-        self.alphabet.remove('1')
+        # Убираю из алфавита пустой переход
+        while '1' in self.alphabet:
+            self.alphabet.remove('1')
 
-    self.is_without_epsilon = True
+        self.is_without_epsilon = True
 
 #######################################################################
 
-def nfa_to_dfa(self):
-    # Преобразует NFA без ε-переходов в эквивалентный DFA.
-    if not self.is_without_epsilon:
-        self.remove_epsilon_transitions()
+    def nfa_to_dfa(self):
+        # Преобразует NFA без ε-переходов в эквивалентный DFA.
+        if not self.is_without_epsilon:
+            self.remove_epsilon_transitions()
+            
+        dfa_states = []
+        dfa_transition_function = {}
+        dfa_final_states = set()
+
+        # Начальное состояние DFA - это состояние начального состояния NFA
+        initial_state = frozenset([self.initial_state])
+        dfa_states.append(initial_state)
+
+        # Используем очередь для обхода состояний DFA
+        queue = [initial_state]
+
+        while queue:
+            current_states = queue.pop(0)
+
+            # Проверяем каждый символ алфавита NFA
+            for symbol in self.alphabet:
+                # Находим все состояния, которые можно достичь из current_states по символу
+                next_states = move(current_states, symbol, self.transition_function)
+
+                if next_states:
+                    sorted(next_states)
+                    next_state_frozen = frozenset(next_states)
+
+                    # Добавляем новый набор состояний, если его еще нет в DFA
+                    if next_state_frozen not in dfa_states:
+                        dfa_states.append(next_state_frozen)
+                        queue.append(next_state_frozen)
+
+                    # Добавляем переход в DFA
+                    dfa_transition_function[(current_states, symbol)] = next_state_frozen
+
+            # Проверяем, является ли текущее состояние финальным
+            if any(state in self.final_states for state in current_states):
+                dfa_final_states.add(current_states)
+
+        # Меняем в DFA
+
+        self.states = [set(s) for s in dfa_states]
+        self.transition_function = dfa_transition_function.copy()
+        self.initial_state = initial_state
+        self.final_states = dfa_final_states.copy()
+        self.is_dfa = True
+        self.rename_dfa_states()
+
+#######################################################################
+
+    def fulling_dfa(self):
+        # ДКА переводит в ПДКА
+        # Если не ДКА, переводит в ДКА
+        if not self.is_dfa:
+            self.nfa_to_dfa()
         
-    dfa_states = []
-    dfa_transition_function = {}
-    dfa_final_states = set()
-
-    # Начальное состояние DFA - это состояние начального состояния NFA
-    initial_state = frozenset([self.initial_state])
-    dfa_states.append(initial_state)
-
-    # Используем очередь для обхода состояний DFA
-    queue = [initial_state]
-
-    while queue:
-        current_states = queue.pop(0)
-
-        # Проверяем каждый символ алфавита NFA
-        for symbol in self.alphabet:
-            # Находим все состояния, которые можно достичь из current_states по символу
-            next_states = move(current_states, symbol, self.transition_function)
-
-            if next_states:
-                sorted(next_states)
-                next_state_frozen = frozenset(next_states)
-
-                # Добавляем новый набор состояний, если его еще нет в DFA
-                if next_state_frozen not in dfa_states:
-                    dfa_states.append(next_state_frozen)
-                    queue.append(next_state_frozen)
-
-                # Добавляем переход в DFA
-                dfa_transition_function[(current_states, symbol)] = next_state_frozen
-
-        # Проверяем, является ли текущее состояние финальным
-        if any(state in self.final_states for state in current_states):
-            dfa_final_states.add(current_states)
-
-    # Меняем в DFA
-
-    self.states = [set(s) for s in dfa_states]
-    self.transition_function = dfa_transition_function.copy()
-    self.initial_state = initial_state
-    self.final_states = dfa_final_states.copy()
-    self.is_dfa = True
-    self.rename_dfa_states()
-
-#######################################################################
-
-def fulling_dfa(self):
-    # ДКА переводит в ПДКА
-    # Если не ДКА, переводит в ДКА
-    if not self.is_dfa:
-        self.nfa_to_dfa()
-    
-    # Добавляем состояние TRASH
-    if 'TRASH' not in self.states:
-        self.states.append('TRASH')
-    
-    # Добавляем переходы по всем буквам
-    for state in self.states:
-        for symbol in self.alphabet:
-            if (state, symbol) not in self.transition_function:
-                self.transition_function.setdefault((state, symbol), set()).add('TRASH')
-    
-    self.is_full_dka = True
+        # Добавляем состояние TRASH
+        if 'TRASH' not in self.states:
+            self.states.append('TRASH')
+        
+        # Добавляем переходы по всем буквам
+        for state in self.states:
+            for symbol in self.alphabet:
+                if (state, symbol) not in self.transition_function:
+                    self.transition_function.setdefault((state, symbol), set()).add('TRASH')
+        
+        self.is_full_dka = True
 
 ############################################################################################################
 
